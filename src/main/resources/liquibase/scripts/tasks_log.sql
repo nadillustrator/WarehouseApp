b703731d7fdc20f7f2e97a0d5af2f0fff212fc4a
nadillustrator <nadin2208@gmail.com>
2023-03-30 22:00:16 +0300

    create table

--- src/main/resources/liquibase/scripts/tasks_log.sql
+++ src/main/resources/liquibase/scripts/tasks_log.sql
@@ -1,2 +1,14 @@
 -- liquibase formatted sql
 
+-- changeSet nadillustrator:1
+CREATE TABLE socks
+(
+    id              SERIAL  NOT NULL PRIMARY KEY,
+    color           TEXT NOT NULL,
+    cotton_part     SMALLINT NOT NULL,
+    quantity        NUMERIC
+);
+
+-- changeSet nadillustrator:2
+ALTER TABLE socks ALTER COLUMN quantity TYPE BIGINT;
+ALTER TABLE socks ALTER COLUMN quantity SET NOT NULL;
