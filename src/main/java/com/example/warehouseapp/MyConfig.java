5f1d73494661067ff9198a8802ce5c6b00abedec
nadillustrator <nadin2208@gmail.com>
2023-03-30 21:25:21 +0300

    start prj

---
+++ src/main/java/com/example/warehouseapp/MyConfig.java
@@ -0,0 +1,11 @@
+package com.example.warehouseapp;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.example.warehouseapp")
+public class MyConfig {
+
+
+}
